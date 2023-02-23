1d72db2a3874b07203249f4a2084b1c4810c362a
vishvajith17 <maduka.18@cse.mrt.ac.lk>
2023-02-23 19:59:40 +0530

    objects define

---
+++ object.js
@@ -0,0 +1,48 @@
+console.log("Object");
+//object define with literals
+const circle = {
+    radius : 4,
+    location : {
+        x_cordinate : 3,
+        y_cordinate : 3,
+    },
+    color : "red",
+    draw : function(radius){
+        console.log("draw circle " + radius + " with literal");
+    }
+};
+
+circle.draw(5);
+
+//object define with factory function (better)
+function createCircle(radius){
+    return {
+        radius : radius,
+        draw : function(){
+            console.log("draw circle " + radius + " with factory function")
+        }
+    }
+};
+
+const newCircle = createCircle(2);
+newCircle.draw();
+
+//object define with constructor (better)
+function Circle(radius){
+    this.radius = radius;
+    this.draw = function(){
+        console.log("draw circle " + radius + " wuth constructor function");
+    }
+};
+
+const constructorCircle = new Circle(4);
+constructorCircle.draw();
+
+//also can call like this 
+Circle.call({}, 4)
+Circle.apply({}, [3]);
+
+//****in javascript functions are objects//
+
+
+
